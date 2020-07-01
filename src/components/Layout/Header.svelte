a1bb0eca449face19cf882d44f359c5f687da124
Mohammad Ashour <ashour.mohammad@gmail.com>
2020-07-01 14:54:08 +0100

    Installing and bootstrapping svelte-i18n. Basic translation.

--- src/components/Layout/Header.svelte
+++ src/components/Layout/Header.svelte
@@ -1,9 +1,13 @@
+<script>
+  import { _ } from "svelte-i18n";
+</script>
+
 <header class="hero">
     <div class="hero-body">
         <div class="contiainer">
-            <h1 class="title">Rebel Voter</h1>
+            <h1 class="title">{$_("app_title")}</h1>
 
-            <h2 class="subtitle">Your favorite Star Wars characters</h2>
+            <h2 class="subtitle">{$_("app_slogan")}</h2>
         </div>
     </div>
 </header>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
+import { addMessages, init } from "svelte-i18n";
 import App from "./App.svelte";
+
+import en from "./lang/en.json";
+import ar from "./lang/ar.json";
+
+addMessages("en", en);
+addMessages("ar", ar);
+
+init({
+  fallbackLocale: "en",
+  initialLocale: "ar",
+});
 
 const app = new App({
   target: document.body,
