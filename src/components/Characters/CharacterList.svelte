e841742df2a50136987ad5a73a430fad7dec7165
Mohammad Ashour <ashour.mohammad@gmail.com>
2020-07-02 12:57:48 +0100

    Adding Arabic character data

--- src/components/Characters/CharacterList.svelte
+++ src/components/Characters/CharacterList.svelte
@@ -1,24 +1,25 @@
 <script>
-    import Character from './Character.svelte';
+  import { locale } from "../../services/i18n";
+  import Character from './Character.svelte';
 
-    function fetchCharacters() {
-        return fetch('/data/characters.json')
-                .then(response => response.json());
-    }
+  function fetchCharacters() {
+      return fetch(`/data/${$locale}.json`)
+              .then(response => response.json());
+  }
 </script>
 
 {#await fetchCharacters()}
-    <p>Loading...</p>
+  <p>Loading...</p>
 {:then characters}
-    <div class="columns is-mobile is-multiline">
-        {#each characters as character}
-            <div
-              class="column is-one-third-desktop is-half-tablet is-full-mobile"
-            >
-                <Character {character} />
-            </div>
-        {/each}
-    </div>
+  <div class="columns is-mobile is-multiline">
+    {#each characters as character}
+      <div
+        class="column is-one-third-desktop is-half-tablet is-full-mobile"
+      >
+          <Character {character} />
+      </div>
+    {/each}
+  </div>
 {:catch error}
-    <p>There was a problem loading characters.</p>
+  <p>There was a problem loading characters.</p>
 {/await}
