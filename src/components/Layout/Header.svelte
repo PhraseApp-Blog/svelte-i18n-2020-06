f83175132d02ddfbbcce52bb8394c1d9d7213c0b
Mohammad Ashour <ashour.mohammad@gmail.com>
2020-07-02 19:25:05 +0100

    Fixing CSS class in Header

--- src/components/Layout/Header.svelte
+++ src/components/Layout/Header.svelte
@@ -1,6 +1,6 @@
 <header class="hero">
     <div class="hero-body">
-        <div class="contiainer">
+        <div class="container">
             <h1 class="title">Rebel Voter</h1>
 
             <h2 class="subtitle">Your favorite Star Wars characters</h2>
